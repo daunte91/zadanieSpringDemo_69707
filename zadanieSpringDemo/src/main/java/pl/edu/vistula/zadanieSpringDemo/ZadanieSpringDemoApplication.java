819509752f31220f82bc1a5279b77d05040b5a36
chest <michal.serwatka91@gmail.com>
2025-01-26 21:49:37 +0100

    Initial commit

---
+++ zadanieSpringDemo/src/main/java/pl/edu/vistula/zadanieSpringDemo/ZadanieSpringDemoApplication.java
@@ -0,0 +1,13 @@
+package pl.edu.vistula.zadanieSpringDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZadanieSpringDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZadanieSpringDemoApplication.class, args);
+	}
+
+}
---
+++ zadanieSpringDemo/src/test/java/pl/edu/vistula/zadanieSpringDemo/ZadanieSpringDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.edu.vistula.zadanieSpringDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZadanieSpringDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
